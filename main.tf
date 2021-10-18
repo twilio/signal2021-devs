490b10942ce72d6258bbaba9e6953bb4b10509f6
Dominik Kundel <dkundel@twilio.com>
2021-10-17 22:37:09 -0700

    ci: add github actions & terraform

---
+++ main.tf
@@ -0,0 +1,84 @@
+terraform {
+  required_providers {
+    twilio = {
+      source  = "twilio/twilio"
+      version = "0.8.1"
+    }
+  }
+
+  backend "remote" {
+    organization = "twilio-demos"
+
+    workspaces {
+      name = "talon-1"
+    }
+  }
+}
+
+provider "twilio" {
+  # Configuration options
+}
+
+resource "twilio_serverless_services_v1" "service" {
+  friendly_name       = "Talon Mission"
+  unique_name         = "talon-mission"
+  include_credentials = true
+}
+
+resource "twilio_serverless_services_environments_v1" "environment" {
+  service_sid   = twilio_serverless_services_v1.service.sid
+  unique_name   = "stage-environment"
+  domain_suffix = "stage"
+}
+
+locals {
+  base_domain        = twilio_serverless_services_environments_v1.environment.domain_name
+  base_functions_url = "https://${local.base_domain}"
+  sms_webhook_url    = "${local.base_functions_url}/incoming-sms"
+}
+
+resource "twilio_api_accounts_incoming_phone_numbers_v2010" "phone_number" {
+  area_code     = "415"
+  friendly_name = "Talon 1 Phone Number"
+  sms_url       = local.sms_webhook_url
+  voice_url     = null
+}
+
+resource "twilio_messaging_services_v1" "service" {
+  friendly_name                 = "Talon 1 Messaging Service"
+  use_inbound_webhook_on_number = false
+}
+
+resource "twilio_messaging_services_phone_numbers_v1" "number" {
+  service_sid      = twilio_messaging_services_v1.service.sid
+  phone_number_sid = twilio_api_accounts_incoming_phone_numbers_v2010.phone_number.sid
+}
+
+resource "twilio_verify_services_v2" "service" {
+  friendly_name = "Talon 1"
+}
+
+output "serverlessService" {
+  value     = twilio_serverless_services_v1.service.sid
+  sensitive = true
+}
+
+output "serverlessEnvironmentSuffix" {
+  value     = twilio_serverless_services_environments_v1.environment.domain_suffix
+  sensitive = true
+}
+
+output "serverlessEnvironment" {
+  value     = twilio_serverless_services_environments_v1.environment.sid
+  sensitive = true
+}
+
+output "twilioNumber" {
+  value     = twilio_api_accounts_incoming_phone_numbers_v2010.phone_number.phone_number
+  sensitive = true
+}
+
+output "verifyServiceSid" {
+  value     = twilio_verify_services_v2.service.sid
+  sensitive = true
+}
---
+++ scripts/deploy.bash
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+shopt -s expand_aliases
+
+SERVICE_SID=$(terraform output -raw serverlessService)
+TWILIO_PHONE_NUMBER=$(terraform output -raw twilioNumber)
+VERIFY_SERVICE_SID=$(terraform output -raw verifyServiceSid)
+SERVERLESS_ENVIRONMENT=$(terraform output -raw serverlessEnvironment)
+
+npm run build
+npx twilio-run deploy \
+  --username "$TWILIO_API_KEY" \
+  --password "$TWILIO_API_SECRET" \
+  --service-sid "$SERVICE_SID" \
+  --environment "$SERVERLESS_ENVIRONMENT" \
+  --env .env.example \
+  --load-system-env
+
+alias env-set='npx twilio-run env set --username "$TWILIO_API_KEY" --password "$TWILIO_API_SECRET" --service-sid "$SERVICE_SID" --environment "$SERVERLESS_ENVIRONMENT"'
+
+env-set --key PASSCODE --value $PASSCODE
+env-set --key TWILIO_PHONE_NUMBER --value $TWILIO_PHONE_NUMBER
+env-set --key VERIFY_SERVICE_SID --value $VERIFY_SERVICE_SID
